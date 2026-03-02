07972f5742cd47515f928c4d7122af98d5139df5
irfan <irfan.rajpoot2735@gmail.com>
2026-03-01 17:16:13 -0800

    endpoint set

--- Frontend/src/services/Endpoint.js
+++ Frontend/src/services/Endpoint.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-// export const BaseUrl='https://blog-app-1-xtt3.onrender.com'
-export const BaseUrl='http://localhost:5000'
+export const BaseUrl='https://blog-project-jz9k.onrender.com/'
+// export const BaseUrl='http://localhost:5000'
 const instance = axios.create({
     baseURL:BaseUrl,
     // headers: {
